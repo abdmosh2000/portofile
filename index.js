97ebb21be380af1a9706d85dcea1f5d097a2328f
Abdallah Shalat <85385315+abdmosh2000@users.noreply.github.com>
2023-05-14 17:46:58 +0300

    up

---
+++ index.js
@@ -0,0 +1,21 @@
+let up = document.getElementById('up');
+
+onscroll =function() {
+    if (scrollY>=100){
+        up.style.display = "block";
+
+    }
+    else{
+            up.style.display = "none";
+            
+        }
+
+
+}
+up.onclick=function(){
+  scroll({
+    top: 0,
+    left: 0,
+    behavior:'smooth'
+  });
+};
